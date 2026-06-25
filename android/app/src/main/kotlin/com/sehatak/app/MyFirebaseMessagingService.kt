37edd9f7b7cab2d1e3daefa05a96ebdf4de52ac7
almwld <almwld@github.com>
2026-06-25 07:17:18 +0300

    🐛 إصلاح MyFirebaseMessagingService.kt - إضافة الاستيرادات الصحيحة

--- android/app/src/main/kotlin/com/sehatak/app/MyFirebaseMessagingService.kt
+++ android/app/src/main/kotlin/com/sehatak/app/MyFirebaseMessagingService.kt
@@ -4,6 +4,7 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         // معالجة الرسالة
