0f02228e547d1101c6c5c484f4acdd0c526bc883
almwld <almwld@github.com>
2026-07-09 06:50:19 +0300

    🔥 إصلاح شامل ونهائي لأخطاء الـ Imports، الـ Null-Safety، والـ Localizations

--- lib/presentation/bloc/chat_bloc/chat_bloc.dart
+++ lib/presentation/bloc/chat_bloc/chat_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sehatak/core/services/chat_service.dart';
