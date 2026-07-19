5e4304f356853c8b0f95e126236ebe7efa70b31d
almwld <almwld@github.com>
2026-07-19 21:55:58 +0300

    🐛 إصلاح أخطاء Colors و HomeScreen في home_screen.dart و auth_screen.dart

--- lib/presentation/screens/auth/auth_screen.dart
+++ lib/presentation/screens/auth/auth_screen.dart
@@ -1,3 +1,4 @@
+import "package:sehatak/presentation/screens/home/home_screen.dart";
 import "package:flutter/material.dart";
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import "package:flutter/material.dart";
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:carousel_slider/carousel_slider.dart';
