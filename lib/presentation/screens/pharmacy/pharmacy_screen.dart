32f456ddf7443fec2062b787a88736899396c222
almwld <almwld@github.com>
2026-08-06 11:06:43 +0300

    🔧 إصلاح استيراد CartScreen في PharmacyScreen

--- lib/presentation/screens/pharmacy/pharmacy_screen.dart
+++ lib/presentation/screens/pharmacy/pharmacy_screen.dart
@@ -1,3 +1,4 @@
+import 'package:sehatak/presentation/screens/pharmacy/cart_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
