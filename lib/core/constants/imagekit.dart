e6cf4dc98c2a12a07ca5a45a9e2d080fa1771e14
almwld <152663389+almwld@users.noreply.github.com>
2026-09-07 01:03:57 +0300

    feat(labs): expose 15 ImageKit lab image URLs

--- lib/core/constants/imagekit.dart
+++ lib/core/constants/imagekit.dart
@@ -1,9 +1,6 @@
 class ImageKit {
   static const String baseUrl = 'https://ik.imagekit.io/fqcynk86c';
 
-  // ============================================================
-  // 🖼️ صور الأطباء
-  // ============================================================
   static const String doctor1 = '$baseUrl/images/doctors/doctor_1.png';
   static const String doctor2 = '$baseUrl/images/doctors/doctor_2.png';
   static const String doctor3 = '$baseUrl/images/doctors/doctor_3.png';
@@ -11,9 +8,6 @@ class ImageKit {
   static const String doctor5 = '$baseUrl/images/doctors/doctor_5.png';
   static const String doctor6 = '$baseUrl/images/doctors/doctor_6.png';
 
-  // ============================================================
-  // 🏥 صور المستشفيات
-  // ============================================================
   static const String hospital1 = '$baseUrl/images/hospitals/hospital_1.png';
   static const String hospital2 = '$baseUrl/images/hospitals/hospital_2.png';
   static const String hospital3 = '$baseUrl/images/hospitals/hospital_3.png';
@@ -21,50 +15,44 @@ class ImageKit {
   static const String hospital5 = '$baseUrl/images/hospitals/hospital_5.png';
   static const String hospital6 = '$baseUrl/images/hospitals/hospital_6.png';
 
-  // ============================================================
-  // 🔬 صور المختبرات
-  // ============================================================
-  static const String lab1 = '$baseUrl/images/labs/lab_1.png';
-  static const String lab2 = '$baseUrl/images/labs/lab_2.png';
-  static const String lab3 = '$baseUrl/images/labs/lab_3.png';
+  static const String lab1 = '$baseUrl/images/labs/lab_1.jpg';
+  static const String lab2 = '$baseUrl/images/labs/lab_2.jpg';
+  static const String lab3 = '$baseUrl/images/labs/lab_3.jpg';
+  static const String lab4 = '$baseUrl/images/labs/lab_4.jpg';
+  static const String lab5 = '$baseUrl/images/labs/lab_5.jpg';
+  static const String lab6 = '$baseUrl/images/labs/lab_6.jpg';
+  static const String lab7 = '$baseUrl/images/labs/lab_7.jpg';
+  static const String lab8 = '$baseUrl/images/labs/lab_8.jpg';
+  static const String lab9 = '$baseUrl/images/labs/lab_9.jpg';
+  static const String lab10 = '$baseUrl/images/labs/lab_10.jpg';
+  static const String lab11 = '$baseUrl/images/labs/lab_11.jpg';
+  static const String lab12 = '$baseUrl/images/labs/lab_12.jpg';
+  static const String lab13 = '$baseUrl/images/labs/lab_13.jpg';
+  static const String lab14 = '$baseUrl/images/labs/lab_14.jpg';
+  static const String lab15 = '$baseUrl/images/labs/lab_15.jpg';
+  static const List<String> labList = [lab1, lab2, lab3, lab4, lab5, lab6, lab7, lab8, lab9, lab10, lab11, lab12, lab13, lab14, lab15];
 
-  // ============================================================
-  // 💊 صور الأدوية
-  // ============================================================
   static const String medicine1 = '$baseUrl/images/medicines/medicine_1.png';
   static const String medicine2 = '$baseUrl/images/medicines/medicine_2.png';
   static const String medicine3 = '$baseUrl/images/medicines/medicine_3.png';
   static const String medicine4 = '$baseUrl/images/medicines/medicine_4.png';
 
-  // ============================================================
-  // 🏪 صور الصيدليات
-  // ============================================================
   static const String pharmacy1 = '$baseUrl/images/pharmacies/pharmacy_1.png';
   static const String pharmacy2 = '$baseUrl/images/pharmacies/pharmacy_2.png';
   static const String pharmacy3 = '$baseUrl/images/pharmacies/pharmacy_3.png';
 
-  // ============================================================
-  // 📰 صور المقالات والمنشورات الصحية
-  // ============================================================
   static const String morningWalk = '$baseUrl/images/posts/morning_walk.png';
   static const String immuneBoost = '$baseUrl/images/posts/immune_boost.png';
   static const String sleepTips = '$baseUrl/images/posts/sleep_tips.png';
   static const String skinCare = '$baseUrl/images/posts/skin_care.png';
   static const String nutritionTips = '$baseUrl/images/posts/nutrition_tips.png';
 
-  // ============================================================
-  // 📸 البانرات
-  // ============================================================
   static const String banner1 = '$baseUrl/images/banners/banner_1.png';
   static const String banner2 = '$baseUrl/images/banners/banner_2.png';
   static const String banner3 = '$baseUrl/images/banners/banner_3.png';
   static const String banner4 = '$baseUrl/images/banners/banner_4.png';
-
   static const List<String> bannerList = [banner1, banner2, banner3, banner4];
 
-  // ============================================================
-  // 💬 أيقونات الدردشة
-  // ============================================================
   static const String chatPhoneCall = '$baseUrl/images/chat/phone_call.png';
   static const String chatVideoCall = '$baseUrl/images/chat/video_call.png';
   static const String chatBubble = '$baseUrl/images/chat/chat_bubble.png';
@@ -73,9 +61,6 @@ class ImageKit {
   static const String chatAudioRecord = '$baseUrl/images/chat/audio_record.png';
   static const String chatPlayButton = '$baseUrl/images/chat/play_button.png';
 
-  // ============================================================
-  // 🚚 أيقونات التوصيل
-  // ============================================================
   static const String deliverySehatak = '$baseUrl/images/delivery/delivery_1.png';
   static const String deliveryNas = '$baseUrl/images/delivery/delivery_2.png';
   static const String deliveryTasheel = '$baseUrl/images/delivery/delivery_3.png';
