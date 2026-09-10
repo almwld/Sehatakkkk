bf9017ded2ff61f00d1c35dbec280602dc127056
almwld <152663389+almwld@users.noreply.github.com>
2026-09-11 01:13:38 +0300

    fix(build): import unawaited for call sound shutdown

--- lib/core/services/call_service.dart
+++ lib/core/services/call_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -98,9 +99,7 @@ class CallService {
     });
     _inCall = active;
     _current = active ? id : null;
-    if (!active) {
-      unawaited(CallSoundCoordinator.instance.stopForCall(id));
-    }
+    if (!active) unawaited(CallSoundCoordinator.instance.stopForCall(id));
     return c;
   }
 
