078997603184001ee7bb29b1c29c8dc8fe14dc98
almwld <152663389+almwld@users.noreply.github.com>
2026-09-11 19:51:55 +0300

    fix(analyze): restore ambulance request screen

---
+++ lib/presentation/screens/ambulance/ambulance_request_screen.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class AmbulanceRequestScreen extends StatelessWidget { const AmbulanceRequestScreen({super.key}); @override Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: const Text('طلب إسعاف')), body: const Center(child: Text('خدمة الإسعاف متاحة من خلال مركز الطوارئ'))); }
