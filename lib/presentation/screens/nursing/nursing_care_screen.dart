630c5138397174ffaee2529fc292ac5f880e6446
almwld <152663389+almwld@users.noreply.github.com>
2026-09-11 19:52:02 +0300

    fix(analyze): restore nursing care screen

---
+++ lib/presentation/screens/nursing/nursing_care_screen.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class NursingCareScreen extends StatelessWidget { const NursingCareScreen({super.key}); @override Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: const Text('تمريض منزلي')), body: const Center(child: Text('خدمة التمريض المنزلي'))); }
