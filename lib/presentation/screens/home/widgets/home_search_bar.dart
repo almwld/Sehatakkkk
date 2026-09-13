767deebca394a6b1f9b98577c033d18d3ba14d3f
almwld <152663389+almwld@users.noreply.github.com>
2026-09-13 03:18:50 +0300

    refactor(home): use unified search bar geometry

--- lib/presentation/screens/home/widgets/home_search_bar.dart
+++ lib/presentation/screens/home/widgets/home_search_bar.dart
@@ -1,6 +1,5 @@
-import 'package:sehatak/presentation/widgets/common/custom_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:sehatak/core/constants/app_colors.dart';
+import 'package:sehatak/presentation/widgets/unified_search_bar.dart';
 
 class HomeSearchBar extends StatelessWidget {
   final TextEditingController? controller;
@@ -19,50 +18,19 @@ class HomeSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
+    final iconColor = isDark ? Colors.grey[400]! : Colors.grey[500]!;
 
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      decoration: BoxDecoration(
-        color: isDark ? const Color(0xFF1A2540) : Colors.grey[100],
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(
-          color: isDark ? Colors.grey[800]! : Colors.grey[300]!,
-          width: 1,
-        ),
-      ),
-      child: TextField(
-        controller: controller,
-        onChanged: onChanged,
-        onTap: onTap,
-        readOnly: readOnly,
-        style: TextStyle(
-          color: isDark ? Colors.white : Colors.black87,
-          fontSize: 14,
-        ),
-        textAlign: TextAlign.right,
-        decoration: InputDecoration(
-          hintText: 'ابحث عن طبيب، دواء، أو مستشفى...',
-          hintStyle: TextStyle(
-            color: isDark ? Colors.grey[500] : Colors.grey[400],
-            fontSize: 14,
-          ),
-          prefixIcon: Icon(
-            Icons.search,
-            color: isDark ? Colors.grey[400] : Colors.grey[500],
-          ),
-          suffixIcon: Icon(
-            Icons.mic,
-            color: isDark ? Colors.grey[400] : Colors.grey[500],
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: Colors.transparent,
-          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        ),
-      ),
+    return UnifiedSearchBar(
+      controller: controller,
+      onChanged: onChanged,
+      onTap: onTap,
+      readOnly: readOnly,
+      hint: 'ابحث عن طبيب، دواء، أو مستشفى...',
+      backgroundColor: isDark ? const Color(0xFF1A2540) : Colors.grey[100]!,
+      borderColor: isDark ? Colors.grey[800]! : Colors.grey[300]!,
+      textColor: isDark ? Colors.white : Colors.black87,
+      hintColor: iconColor,
+      suffixIcon: Icon(Icons.mic, color: iconColor),
     );
   }
 }
