0f969c84e9d63ddbcd27313346647c6e73ef0fdb
almwld <152663389+almwld@users.noreply.github.com>
2026-09-16 03:23:20 +0300

    fix(services): import go_router for connected service navigation

--- lib/presentation/screens/services/services_screen.dart
+++ lib/presentation/screens/services/services_screen.dart
@@ -1,5 +1,6 @@
 import 'package:sehatak/presentation/widgets/common/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sehatak/core/constants/app_colors.dart';
 import 'package:sehatak/app_router.dart';
 
